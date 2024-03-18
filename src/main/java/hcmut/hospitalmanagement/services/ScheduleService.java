bc34b2b9f49b2818d14585df75ef1f33f914a19e
LeHoangKhanhVinh <khanhvinh1231232@gmail.com>
2024-03-18 21:08:58 +0700

    build(update-Doctor-and-add-Schedule): add repository, service for Doctor and Schedule

---
+++ src/main/java/hcmut/hospitalmanagement/services/ScheduleService.java
@@ -0,0 +1,39 @@
+package hcmut.hospitalmanagement.services;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import hcmut.hospitalmanagement.models.Schedule;
+import hcmut.hospitalmanagement.repositories.ScheduleRepository;
+
+@Service
+public class ScheduleService implements SService {
+    private final ScheduleRepository scheduleRepository;
+
+    // Constructor
+    @Autowired
+    public ScheduleService(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
+
+
+    // Implementations of Methods in SService
+    
+    // This @Scheduled will automatically execute the below method every minutes
+    // so we will delete the schedule if it has exprired
+    @Scheduled(cron = "0 * * * * ?")
+    public void removeExpiredSchedules() {
+        LocalDateTime now = LocalDateTime.now();
+        List<Schedule> schedules = scheduleRepository.findAll();
+        for (Schedule schedule : schedules) {
+            if (schedule.getEndTime().isBefore(now)) {
+                scheduleRepository.delete(schedule);
+            }
+        }
+    }
+}
