27e00e68a2d0993349ba7f2537db51118c58b250
NM Hung <hnm142758@gmail.com>
2024-04-05 13:54:32 +0700

    docs(update-homepage): add 1 more widget item, add hospital logo and name

--- src/main/resources/static/js/homePage.js
+++
@@ -1,7 +0,0 @@
-var button = document.getElementById('menu-bar');
-var menu = document.querySelector('.option-bar').classList;
-
-button.onclick = function(){
-    menu.toggle("invi");
-    menu.toggle("vi");
-}
